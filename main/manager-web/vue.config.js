974fe6c3b72339c336d9bda1261b9985c747afb6
zhuoqinglian <1035449612@qq.com>
2026-01-23 15:29:43 +0800

    feat: 添加语音盒子在线烧录功能

--- main/manager-web/vue.config.js
+++ main/manager-web/vue.config.js
@@ -44,7 +44,7 @@ module.exports = defineConfig({
     port: 8001, // 指定端口为 8001
     proxy: {
       '/xiaozhi': {
-        target: 'http://127.0.0.1:8002',
+        target: 'http://test.aiot.xin-nan.com',
         changeOrigin: true
       }
     },
